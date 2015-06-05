2891d93357dc1699098a46ce10f86ef38987217d
railsr <ikll7@yahoo.com>
2015-06-05 16:12:46 +0400

    css and helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+
+  def markdown(text)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+     extensions = {
+       autolink: true,
+       tables: true,
+       no_intraemphasis: true,
+       hard_wrap: true    
+     })
+    markdown.render(text).html_safe
+  end
+
+
+  def lab(attr)
+    case attr 
+      when "Mini" 
+        "#148DC8;"
+      when "Mid" 
+         "#F1684D;"
+      when "Pro" 
+         "#D94B61;"
+      when "Laptop" 
+         "#6E68D1;"
+      else "#c6c6cf"
+    end
+  end
 end
