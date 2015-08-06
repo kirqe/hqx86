376e493960f9a3c36f16968eb8cfbc99c21c3177
railsr <ikll7@yahoo.com>
2015-08-06 20:43:40 +0400

    edits

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,10 @@
 
-set :output, "#{path}/log/cron.log"
 
+job_type :rake, "{ cd #{@current_path} > /dev/null; } && RAILS_ENV=:environment bundle exec rake :task --silent :output"
+job_type :script, "{ cd #{@current_path} > /dev/null; } && RAILS_ENV=:environment bundle exec script/:task :output"
+job_type :runner, "{ cd #{@current_path} > /dev/null; } && RAILS_ENV=:environment bundle exec rails runner ':task' :output"
+
+set :output, "#{path}/log/cron.log"
 
 every 1day do  
   rake "sitemap:generate"        
@@ -10,3 +14,4 @@ end
 # every :reboot do
 #   command "sidekiq -q mailer -q default"
 # end
+
