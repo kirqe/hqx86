abb015b3fb8566898fb99e606ff655f904b1c701
railsr <ikll7@yahoo.com>
2015-08-19 14:39:21 +0400

    tag edits + robots

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -33,11 +33,5 @@ module ApplicationHelper
 
     def tag_links(tags)
       tags.split(",").map{|tag| link_to tag.strip, tag_path(tag.strip) }.join(", ") 
-    end
-
-    def tag_cloud(tags)
-      tags.each do |tag|
-        yield(tag)
-      end
-    end      
-  end
+    end        
+end
