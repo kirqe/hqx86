5fd25fcab377cb6611bdeeeaf3205f8354246538
railsr <ikll7@yahoo.com>
2015-12-14 14:15:33 +0400

    ...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { host: 'hqx86.com'}
-  config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
+  config.assets.precompile += ['*.css', '*.js']
 end
