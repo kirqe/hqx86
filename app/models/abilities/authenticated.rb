f5f5b110b6fc143ed392688b533a0ce2beb7b758
railsr <ikll7@yahoo.com>
2015-12-14 05:44:34 +0400

    edits

--- app/models/abilities/authenticated.rb
+++ app/models/abilities/authenticated.rb
@@ -3,9 +3,9 @@ module Abilities
     include CanCan::Ability
 
     def initialize(user)
-      can [:create], [Build, Post, Comment]
+      can [:create], [Build, Comment]
       can [:read, :update], Build, user_id: user.id
-      can [:read, :update], Post, user_id: user.id
+#      can [:read, :update], Post, user_id: user.id
       can :update, User do |u|
          u.id == user.id
       end
