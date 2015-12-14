1a4f4d3b87213c9cdf79245a712a6aae0d9c6aa3
railsr <ikll7@yahoo.com>
2015-12-14 17:16:38 +0400

    edits

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ApplicationMailer
-  def contact_email(name, email, message, request_type, budget, purpose, build_info)
+  def contact_email(name, email, message, request_type)
     @name, @email, @message, @request_type = name, email, message, request_type
     mail(to: ENV['contact_email'], subject: "hqx86 #{@request_type}")
   end
