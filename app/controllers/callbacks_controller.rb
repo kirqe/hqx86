0466e119ac4e7ff0b230f13442198116771c66ab
railsr <ikll7@yahoo.com>
2015-12-17 00:05:21 +0400

    fixes

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,6 +1,6 @@
 class CallbacksController < ApplicationController
   def facebook
-    @user = User.from_oath(request.env["omniauth.auth"])
-    sign_in_and_redirect @user
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    sign_in_and_redirect root_path
   end
 end
