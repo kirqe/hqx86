b1a976453bc03a0ff6e07df1065a3e1a8c02e90b
railsr <ikll7@yahoo.com>
2016-06-30 22:10:06 +0400

    edits

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,17 +34,6 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  private
-
-
-  # def ensure_trailing_slash
-  #   redirect_to url_for(params.merge(:trailing_slash => true)), :status => 301 unless trailing_slash?
-  # end
-  #
-  # def trailing_slash?
-  #   request.env['REQUEST_URI'].match(/[^\?]+/).to_s.last == '/'
-  # end
-
   protected
 
   def configure_permitted_parameters
